82a8cad12701dec07ab880ea67e2c88fcaa303a3
Evelyn Li <evelynli61205@gmail.com>
2021-07-07 19:47:18 -0700

    Add feature to play 1 round

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
 function computerPlay() {
     const outcomes = ['rock', 'paper', 'scissors']
-    return outcomes[Math.floor(Math.random() * 3)]
+    return outcomes[Math.floor(Math.random() * outcomes.length)]
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return 'It\'s a tie!'
+    }
+    else {
+        if (playerSelection === 'rock') {
+            if (computerSelection === 'scissors') {
+                return 'You won! Rock beats Scissors'
+            }
+            return 'You lost! Scissors beats Rock'
+
+        }
+        else if (playerSelection === 'paper') {
+            if (computerSelection === 'rock') {
+                return 'You won! Paper beats Rock'
+            }
+            return 'You lost! Scissors beats Paper'
+
+        }
+        else if (playerSelection === 'scissors') {
+            if (computerSelection === 'paper') {
+                return 'You won! Scissors beats Paper'
+            }
+            return 'You lost! Rock beats Scissors'
+        }
+    }
 }
